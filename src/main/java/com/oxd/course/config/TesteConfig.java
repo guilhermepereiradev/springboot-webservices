e21cf7c0e051c390083b72dd2e6aaa453d83515c
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2022-08-22 22:34:01 -0300

    OrderStatus Enum

--- src/main/java/com/oxd/course/config/TesteConfig.java
+++ src/main/java/com/oxd/course/config/TesteConfig.java
@@ -2,6 +2,7 @@ package com.oxd.course.config;
 
 import com.oxd.course.entities.Order;
 import com.oxd.course.entities.User;
+import com.oxd.course.entities.enums.OrderStatus;
 import com.oxd.course.repositories.OrderRepository;
 import com.oxd.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TesteConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID,u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
