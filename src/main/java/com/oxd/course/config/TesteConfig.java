20af0eab0751cbc8e634bd5e892c3d642fb7d4fc
guilhermepereiradev <guilhermepereira1609@hotmail.com>
2022-08-23 21:37:54 -0300

    Payment, one-to-one association

--- src/main/java/com/oxd/course/config/TesteConfig.java
+++ src/main/java/com/oxd/course/config/TesteConfig.java
@@ -74,5 +74,10 @@ public class TesteConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-07-22T17:21:22Z"), o3);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
